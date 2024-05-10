6b53701a8acefe54823b8e976a58504be2443f5f
AmerRiaz <amer.riaz@gmail.com>
2024-05-10 16:56:21 +1000

    stub functions added to todo_functions.py; import of functions added to main.py

--- main.py
+++ main.py
@@ -2,6 +2,11 @@
 # implementation of the main logic of the app
 
 
+
+# Imports of our own functions from other files
+from todo_functions import add_todo, remove_todo, mark_todo, view_todo
+
+
 # print a welcome message
 print("Welcome to the Shopping list app.")
 
@@ -25,21 +30,22 @@ while choice != "5":
     choice = create_menu()
 
     if (choice == "1"):
-        print("Item added.")
-        # add_todo(file_name)
+        # print("Item added.")
+        add_todo()
     elif (choice == "2"):
-        print("Item removed.")
-        # remove_todo(file_name)
+        # print("Item removed.")
+        remove_todo()
     elif (choice == "3"):
-        print("Item marked.")
-        # mark_todo(file_name)
+        # print("Item marked.")
+        mark_todo()
     elif (choice == "4"):
-        print("List printed.")
-        # view_todo(file_name)
+        # print("List printed.")
+        view_todo()
     elif (choice == "5"):
         print("You entered 5 to exit.")
     else:
         print("Please only enter the options shown above.")
 
 
+# exit the app saying Thank You to user
 print("Thank you for using my Shopping list app.")
--- todo_functions.py
+++ todo_functions.py
@@ -1,2 +1,15 @@
 # todo_functions.py
 # implementation of the features of the app
+
+
+def add_todo():
+    print("Add todo")
+
+def remove_todo():
+    print("Remove todo")
+
+def mark_todo():
+    print("Mark todo")
+
+def view_todo():
+    print("View todo")
